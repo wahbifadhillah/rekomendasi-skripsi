283773a1c7efd1d8c06e337b8320db887db291c7
Wahbi Fadhillah <wahbifadhillah@gmail.com>
2021-10-20 15:57:40 +0700

    heroku pgsql train fix 9

--- app/Node.php
+++ app/Node.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Node extends Model
 {
     protected $primaryKey = null;
-    public $incrementing = false;
-    public $timestamps = true;
     protected $table = 'nodes';
     protected $fillable = [
     'node_id', 'tree_id', 'node_parent', 'node_name','node_value', 'node_leaf_purity'];
